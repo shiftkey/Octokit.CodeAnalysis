e53f4e2a9d3fea4fb0c6e897ec25c5d8cd2c8986
Brendan Forster <brendan@github.com>
2015-12-18 15:34:21 +1030

    throw in some more tests

--- Octokit.CodeAnalysis/Octokit.CodeAnalysis.Test/UnitTests.cs
+++ Octokit.CodeAnalysis/Octokit.CodeAnalysis.Test/UnitTests.cs
@@ -49,6 +49,36 @@ namespace Octokit.CodeAnalysis.Test
         }
 
         [TestMethod]
+        public void HappyPathWorksWithInlineDeclaration()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace Octokit
+    {
+        public class IssueEventsClient
+        {   
+            [Endpoint(""repos/:owner/:repo/issues/events"")]
+            public Task<IReadOnlyList<string>> GetAllForRepository(string owner, string name)
+            {
+                Ensure.ArgumentNotNullOrEmptyString(owner, ""owner"");
+                Ensure.ArgumentNotNullOrEmptyString(name, ""name"");
+
+                return ApiConnection.GetAll<string>(""repos/{0}/{1}/issues/events"".FormatUri(owner, name));
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+
+        [TestMethod]
         public void MarkerInterfaceCannotResolveToOldWay()
         {
             var test = @"
@@ -63,7 +93,7 @@ namespace Octokit.CodeAnalysis.Test
     {
         public class IssueEventsClient
         {   
-            [Endpoint(""/repos/:owner/:repo/issues/events"")]
+            [Endpoint(""repos/:owner/:repo/issues/events"")]
             public Task<IReadOnlyList<string>> GetAllForRepository(string owner, string name)
             {
                 Ensure.ArgumentNotNullOrEmptyString(owner, ""owner"");
@@ -87,6 +117,49 @@ namespace Octokit.CodeAnalysis.Test
 
             VerifyCSharpDiagnostic(test, expected);
         }
+
+        [TestMethod]
+        public void ReportErrorWhenResourceIsDifferent()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace Octokit
+    {
+        public class IssueEventsClient
+        {   
+            [Endpoint(""repos/:owner/:repo/events"")]
+            public Task<IReadOnlyList<string>> GetAllForRepository(string owner, string name)
+            {
+                Ensure.ArgumentNotNullOrEmptyString(owner, ""owner"");
+                Ensure.ArgumentNotNullOrEmptyString(name, ""name"");
+
+                var uri = ""repos/{0}/{1}/issues/events"".FormatUri(owner, name);
+
+                return ApiConnection.GetAll<string>(uri);
+            }
+        }
+    }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "Octokit.CodeAnalysis",
+                Message = String.Format("Method '{0}' declares it consumes '{1}' but actually uses '{2}'", "GetAllForRepository", "repos/:owner/:repo/events", "repos/{0}/{1}/issues/events"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 14, 48)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
